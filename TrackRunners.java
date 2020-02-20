97915b686e208c3683e99410af654296fc793def
Terzakis <10951@VTSD.COM>
2020-02-20 08:42:00 -0500

    first day

---
+++ TrackRunners.java
@@ -0,0 +1,8 @@
+public abstract interface TrackRunners{
+  public abstract void practice();
+  public abstract void diet();
+  public abstract void uniform();
+  public abstract void workout();
+  public abstract void shoes();
+  public abstract void equipment();
+}
