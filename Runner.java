157524155ab6b5a1000b3486950e50234adc1fc0
Terzakis <10951@vtsd.com>
2020-02-25 08:14:49 -0500

    update :)

--- Runner.java
+++ Runner.java
@@ -4,27 +4,40 @@ public class Runner{
     FourHundredm test4 = new FourHundredm();
     Jumpers testJ = new Jumpers();
     Throwers testT = new Throwers();
+//sprinters
     testS.uniform();
     testS.highschool();
     testS.coach();
     testS.practice();
-
+    testS.workout();
+    testS.shoes();
+    testS.equipment();
+//400m
     test4.uniform();
     test4.highschool();
     test4.coach();
     test4.practice();
+    test4.workout();
+    test4.shoes();
+    test4.equipment();
 
     //Jumpers
     testJ.uniform();
     testJ.highschool();
     testJ.coach();
     testJ.practice();
+    testJ.workout();
+    testJ.shoes();
+    testJ.equipment();
 
     //Throwers
     testT.uniform();
     testT.highschool();
     testT.coach();
     testT.practice();
+    testT.workout();
+    testT.shoes();
+    testT.equipment();
 
   }
 
